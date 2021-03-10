42fa03c97d65b1b0b6a46428153eddd6f94e9940
Abhishek Bhatti <abhbhatt3@publicisgroupe.net>
2021-03-10 20:52:27 +0530

    adventure app

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import Navbar from '../src/components/Navbar.js';
+import React from 'react';
+import { BrowserRouter as Router , Switch, Route } from 'react-router-dom';
+import './App.css';
+import Home from '../src/components/Pages/Home';
+import Services from '../src/components/Pages/Services';
+import Products from '../src/components/Pages/Products';
+import SignUp from '../src/components/Pages/SignUp';
+
+function App() {
+  return (
+    <>
+      <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/services" component={Services} />
+        <Route path="/products" component={Products} />
+        <Route path="/sign-up" component={SignUp} />
+      </Switch>
+
+      </Router>
+      
+    </>
+  );
+}
+
+export default App;
